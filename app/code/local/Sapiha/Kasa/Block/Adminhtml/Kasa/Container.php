ebc3f552506fa7a42399b24cefff3bff6b24e170
Yurii Sapiha <yurii.sapiha@transoftgroup.com>
2018-08-17 16:59:08 +0300

    refs#40908 #40909 Додати новий метод доставки і метод оплати Т37-v1(module.xml files і удалив кнопку "Add new" на гріді)

---
+++ app/code/local/Sapiha/Kasa/Block/Adminhtml/Kasa/Container.php
@@ -0,0 +1,18 @@
+<?php
+
+class Sapiha_Kasa_Block_Adminhtml_Kasa_Container extends Mage_Adminhtml_Block_Widget_Grid_Container
+{
+    /**
+     * Sapiha_Kasa_Block_Adminhtml_Kasa_Container constructor.
+     */
+    public function __construct()
+    {
+
+        $this->_blockGroup = 'sapiha_kasa';
+        $this->_controller = 'adminhtml_kasa';
+        $this->_headerText = $this->__('Kasa Payment Info');
+
+        parent::__construct();
+        $this->_removeButton('add');
+    }
+}
